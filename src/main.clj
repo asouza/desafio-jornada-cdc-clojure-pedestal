c33d54449899b046e2a9cfc0433b1c3335fb4189
Alberto <alberto.tavares@zup.com.br>
2024-09-21 06:39:46 -0300

    setup do projeto

---
+++ src/main.clj
@@ -0,0 +1,78 @@
+(ns main (:require [io.pedestal.http :as http]
+                   [io.pedestal.http.route :as route]
+                   [io.pedestal.test :as test]))
+
+
+
+(def novo-autor {
+  :name :novo-autor
+  :enter (fn [context]
+           context
+           )
+})
+
+;; configuracoes
+
+(defonce database (atom {}))
+
+(def db-interceptor
+  {
+   :name :database-interceptor
+   :enter (fn [context]
+            (println "Entrando no db-interceptor")
+            (update context :request assoc :database @database)
+            )
+   :leave (fn [context]
+            (println "Saindo do db-interceptor")
+            ;ta recuperando um valor do mapa e desestruturando nas varíaveis. Aqui no caso é a operacao + args
+            (if-let [[op & args] (:tx-data context)]
+              (do
+                ;aqui atualiza o valor do atom aplicando a funcao op com os args passados
+                ;aqui é importante notar que a funcao de atualizacao do atom tem que ser pura.
+                (apply swap! database op args)
+                (assoc-in context [:request :database] @database)
+                )
+              ;; o db interceptor não estava retornando nada quando não tinha operacao. Se o interceptor
+              ;; nao retorna nada ele interrompe o fluxo.
+              context
+              )
+            )
+   }
+  )
+
+;eu quero descobrir pq o argumento de nome da rota não é um mapa
+(def routes
+  (route/expand-routes
+    #{
+      ["/autores" :post [db-interceptor novo-autor]]
+      }
+    )
+  )
+
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8890
+   }
+  )
+
+(defonce server (atom nil))
+
+(defn test-request [verb url]
+  (io.pedestal.test/response-for (::http/service-fn @server) verb url))
+
+(defn start-dev []
+  (reset! server (http/start (http/create-server (assoc service-map ::http/join? false))))
+  )
+
+(defn stop-server []
+  (http/stop @server)
+  )
+
+(defn restart []
+  (stop-server)
+  (start-dev)
+  )
+
+
+
