9466878ba38dee4c102cbd189df72b5c5d0cde5f
Alberto <alberto.tavares@zup.com.br>
2024-10-10 16:46:50 -0300

    [datomic autor] listando autores salvos no banco

--- src/lista_autores.clj
+++ src/lista_autores.clj
@@ -1,6 +1,7 @@
 (ns lista-autores
   (:require
     [utilitarios]
+    [datomic.api :as d]
     )
   )
 
@@ -8,18 +9,35 @@
 ;Ao mesmo tempo disse que meu código está ok
 (defn- converte-linha-autor-saida-lista [autor]
   {
-   :nome (:nome autor)
-   :email (:email autor)
-   :descricao (:descricao autor)
+   :nome (autor 0)
+   :email (autor 1)
+   :descricao (autor 2)
    }
+  )
+
+(defn- busca-autores [dados]
+  (let [
+        query '[:find ?nome ?email ?descricao
+                :where
+                  [?e :autor/email ?email]
+                  [?e :autor/nome ?nome]
+                  [?e :autor/descricao ?descricao]
+                ]
+
+        ]
+    (d/q query dados)
+    )
   )
 
 (def handler
   {
    :name :lista-autores
    :enter (fn [context]
-            (let [linhas-autores (get-in context [:request :database :autores])]
-              (utilitarios/respond-with-json context (map converte-linha-autor-saida-lista linhas-autores))
+            (let [
+                  dados (get-in context [:request :db])
+                  autores (busca-autores dados)
+                  ]
+              (utilitarios/respond-with-json context (map converte-linha-autor-saida-lista autores))
               ))
    }
   )
