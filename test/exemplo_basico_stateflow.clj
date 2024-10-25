4e36056229e7ec656a374c59f6cd62d58f401821
Alberto <alberto.tavares@zup.com.br>
2024-10-25 10:58:14 -0300

    [stateflow] tentando sacar o fluxo e matchers

--- test/exemplo_basico_stateflow.clj
+++ test/exemplo_basico_stateflow.clj
@@ -17,19 +17,23 @@
 
 (def inc-two
   (flow "inc 2 times"
+        get-value
         inc-value
-        inc-value))
+        get-value))
 
 (api/run inc-two {:value 4})
 
-(def with-bindings
+(def with-bindings-teste
   (flow "get double value"
-        inc-value
+        ;; inc-value
         [value get-value
-         :let [value2 (* 2 value)]]
-        (api/return value2)))
+         :let [value2 (* 3 value)]]
+        get-value
+        (api/return value2) 
+        ))
 
-(api/run with-bindings {:value 4})
+(api/run with-bindings-teste {:value 4 :outro 8})
+
 
 (def with-assertions
   (flow "with assertions"
@@ -39,6 +43,27 @@
 
         inc-value
         [world (api/get-state identity)]
-        (match? 7 get-value)))
+        (match? 7 get-value)
+        (match? 1 (count world))))
 
-(api/run with-assertions {:value 4})
+(api/run* {:fail-fast? false :init (constantly {:value 4})} with-assertions)
+
+(def for-flow
+  (flow "even? returns true for even numbers"
+        (api/for [x (filter even? (range 10))]
+          (match? even? x))))
+
+(api/run for-flow)    
+
+(deftest fruits-and-veggies
+  (clojure.test/testing "bla"
+    (flow "surprise! Tomatoes are fruits!"
+          (match? #{:tomato} #{:tomato :potato}))))
+
+
+(api/defflow teste-flow
+  (match? 1 1)
+  )
+
+(deftest teste
+  (clojure.test/is (= 5 5) "mensagem"))
